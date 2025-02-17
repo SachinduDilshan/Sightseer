c567b64235b429c990c568590663a070be698211
K.G.S.D Abeyrathne <sachindud498@gmail.com>
2025-02-18 00:00:30 +0530

    FastAPI

--- src/backend/recommendation_model/main.py
+++ src/backend/recommendation_model/main.py
@@ -1,1 +1,11 @@
-print("Hello")
+from fastapi import FastAPI
+
+app = FastAPI()  # Initialize FastAPI app
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to the Sightseer Trip Recommendation API!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
